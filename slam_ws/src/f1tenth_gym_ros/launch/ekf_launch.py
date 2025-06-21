3db9b244e1e1bf4f5857dcf52cd0fb618bf2ce34
CharlesMcCabe <mccabe.charles@icloud.com>
2025-06-21 17:22:37 -0400

    working slam

---
+++ slam_ws/src/f1tenth_gym_ros/launch/ekf_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    config_path = os.path.join(
+        get_package_share_directory('f1tenth_gym_ros'),
+        'config',
+        'ekf.yaml'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[config_path]
+        )
+    ])
+
