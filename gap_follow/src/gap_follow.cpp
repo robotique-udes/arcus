c67d2377d93b4161cbeb04484fbb901fd0a3d8a5
gabduf <gab.duf04@icloud.com>
2025-09-23 20:36:23 -0400

    devops pipeline + lint apply

--- gap_follow/src/gap_follow.cpp
+++ gap_follow/src/gap_follow.cpp
@@ -7,25 +7,28 @@
 #include <algorithm>
 /// CHECK: include needed ROS msg type headers and libraries
 
-class ReactiveFollowGap : public rclcpp::Node {
-
-public:
-    ReactiveFollowGap() : Node("reactive_node")
+class ReactiveFollowGap : public rclcpp::Node
+{
+  public:
+    ReactiveFollowGap():
+        Node("reactive_node")
     {
-
-    _direction_publisher = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(drive_topic, 10);
-    _laserScan_subscriber = this->create_subscription<sensor_msgs::msg::LaserScan>(lidarscan_topic, 10,
-            [this](const sensor_msgs::msg::LaserScan::SharedPtr msg) {this->lidar_callback(msg);});
-        
+        _direction_publisher = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(drive_topic, 10);
+        _laserScan_subscriber
+            = this->create_subscription<sensor_msgs::msg::LaserScan>(lidarscan_topic,
+                                                                     10,
+                                                                     [this](const sensor_msgs::msg::LaserScan::SharedPtr msg)
+                                                                     {
+                                                                         this->lidar_callback(msg);
+                                                                     });
     }
 
-private:
-
+  private:
     int overshootFactor = 1;
 
     bool STRAIGHT = false;
 
-    float past_best_angle = 0; 
+    float past_best_angle = 0;
     int consecutive_target = 0;
 
     float best_angle = 0;
@@ -41,19 +44,22 @@ private:
     /// TODO: create ROS subscribers and publishers
 
     void preprocess_lidar(std::vector<float>& ranges)
-    {   
+    {
         uint32_t rangesSize = ranges.size();
-        for (uint32_t i = 4; i < rangesSize-4; ++i)
+        for (uint32_t i = 4; i < rangesSize - 4; ++i)
         {
-            ranges[i] = (ranges[i]+ranges[i-1]+ranges[i-2]+ranges[i-3]+ranges[i+1]+ranges[i+2]+ranges[i+3])/7.;
+            ranges[i]
+                = (ranges[i] + ranges[i - 1] + ranges[i - 2] + ranges[i - 3] + ranges[i + 1] + ranges[i + 2] + ranges[i + 3])
+                  / 7.;
 
-        if(ranges[i]>10) ranges[i] = 10; 
+            if (ranges[i] > 10)
+                ranges[i] = 10;
         }
         return;
     }
 
-    void lidar_callback(sensor_msgs::msg::LaserScan::SharedPtr scan_msg) 
-    {   
+    void lidar_callback(sensor_msgs::msg::LaserScan::SharedPtr scan_msg)
+    {
         preprocess_lidar(scan_msg->ranges);
         // Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         /// TODO:
@@ -61,97 +67,93 @@ private:
         std::vector<float> ranges = scan_msg->ranges;
         uint32_t rangesSize = ranges.size();
 
-              
-        if(!STRAIGHT)
+        if (!STRAIGHT)
         {
-
             float min_value = 0;
 
-            if (!ranges.empty()) {
-            min_value = *std::min_element(ranges.begin(), ranges.end());
-            //RCLCPP_INFO(this->get_logger(), "min_val_range: %f", min_value);
-
+            if (!ranges.empty())
+            {
+                min_value = *std::min_element(ranges.begin(), ranges.end());
+                // RCLCPP_INFO(this->get_logger(), "min_val_range: %f", min_value);
             }
-            // Eliminate all points inside 'bubble' (set them to zero) 
+            // Eliminate all points inside 'bubble' (set them to zero)
             uint32_t bubble_radius = 1;
 
             for (uint32_t i = 0; i < rangesSize; i++)
             {
-        
-                if (ranges[i] <= (min_value + bubble_radius))  
+                if (ranges[i] <= (min_value + bubble_radius))
                 {
-                    //RCLCPP_INFO(this->get_logger(), "removed index: %d", i);
+                    // RCLCPP_INFO(this->get_logger(), "removed index: %d", i);
                     ranges[i] = 0.0;  // Mark as invalid (cleared)
                 }
             }
 
-            for(uint32_t i=0; i<4; i++)
+            for (uint32_t i = 0; i < 4; i++)
             {
                 ranges[i] = 0.0;
             }
 
-            for(uint32_t i= rangesSize-4; i<rangesSize; i++)
+            for (uint32_t i = rangesSize - 4; i < rangesSize; i++)
             {
                 ranges[i] = 0.0;
             }
-            // Find max length gap 
+            // Find max length gap
             uint32_t max_gap = 0;
-            uint32_t gap = 0 ;
-            uint32_t startingGapIndex = 0 ;
-            uint32_t endingGapIndex = 0 ;
+            uint32_t gap = 0;
+            uint32_t startingGapIndex = 0;
+            uint32_t endingGapIndex = 0;
 
-                for (uint32_t i = 0; i < rangesSize; i++)
+            for (uint32_t i = 0; i < rangesSize; i++)
+            {
+                if (ranges[i] != 0.0)  // Non-zero value: part of a gap
                 {
-                    if (ranges[i] != 0.0)  // Non-zero value: part of a gap
+                    if (gap == 0)
                     {
-                        if (gap == 0) 
-                        {
-                            // Start of a new gap
-                            startingGapIndex = i;
-                        }
-                        gap++;
-                        endingGapIndex = i;  // Increase the gap count
+                        // Start of a new gap
+                        startingGapIndex = i;
                     }
-                    else 
-                    {
-                        if (gap > max_gap) 
-                        {
-                            max_gap = gap;
-                            MAXstartingGapIndex = startingGapIndex;
-                            MAXendingGapIndex = endingGapIndex;
-                        }
-                        gap = 0;  // Reset gap counter for the next potential gap
-                    }
+                    gap++;
+                    endingGapIndex = i;  // Increase the gap count
                 }
-
-                if (gap > max_gap) 
+                else
                 {
-                    max_gap = gap;
-                    MAXstartingGapIndex = startingGapIndex;
-                    MAXendingGapIndex = endingGapIndex;
+                    if (gap > max_gap)
+                    {
+                        max_gap = gap;
+                        MAXstartingGapIndex = startingGapIndex;
+                        MAXendingGapIndex = endingGapIndex;
+                    }
+                    gap = 0;  // Reset gap counter for the next potential gap
                 }
+            }
 
-            uint32_t best_index = (MAXendingGapIndex-MAXstartingGapIndex)/2. + MAXstartingGapIndex;
+            if (gap > max_gap)
+            {
+                max_gap = gap;
+                MAXstartingGapIndex = startingGapIndex;
+                MAXendingGapIndex = endingGapIndex;
+            }
 
-            best_angle = scan_msg->angle_min + best_index*scan_msg->angle_increment;
+            uint32_t best_index = (MAXendingGapIndex - MAXstartingGapIndex) / 2. + MAXstartingGapIndex;
 
-    
+            best_angle = scan_msg->angle_min + best_index * scan_msg->angle_increment;
+
             past_best_angle = best_angle;
         }
 
         STRAIGHT = false;
 
         ackermann_msgs::msg::AckermannDriveStamped new_msg = ackermann_msgs::msg::AckermannDriveStamped();
-        
-        new_msg.drive.steering_angle = best_angle*overshootFactor;
-        new_msg.drive.speed = 6;  //6 for montreal
 
-        _direction_publisher->publish(new_msg);    
+        new_msg.drive.steering_angle = best_angle * overshootFactor;
+        new_msg.drive.speed = 6;  // 6 for montreal
+
+        _direction_publisher->publish(new_msg);
     }
-
 };
 
-int main(int argc, char ** argv) {
+int main(int argc, char** argv)
+{
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ReactiveFollowGap>());
     rclcpp::shutdown();
--- motor_sweep_test/src/motor_sweep_test.cpp
+++ motor_sweep_test/src/motor_sweep_test.cpp
@@ -6,44 +6,45 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
 
-
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+ * member function as a callback from the timer. */
 
 class MotorSweepTest : public rclcpp::Node
 {
   public:
-    MotorSweepTest()
-    : Node("motor_sweep_test"), count_(0)
+    MotorSweepTest():
+        Node("motor_sweep_test"),
+        count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/motor/duty_cycle", 10);
-      publisher2_ = this->create_publisher<std_msgs::msg::Float64>("commands/servo/position", 10);
-      timer_ = this->create_wall_timer(
-      4000ms, std::bind(&MotorSweepTest::timer_callback, this));
+        publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/motor/duty_cycle", 10);
+        publisher2_ = this->create_publisher<std_msgs::msg::Float64>("commands/servo/position", 10);
+        timer_ = this->create_wall_timer(4000ms, std::bind(&MotorSweepTest::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::Float64();
-      auto servo_msg = std_msgs::msg::Float64();
-      for (int i = 0; i < 10; i++) {
-        message.data = -i/100.0;
-        publisher_->publish(message);
-        servo_msg.data = i/10.0;
-        publisher2_->publish(servo_msg);
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      }
+        auto message = std_msgs::msg::Float64();
+        auto servo_msg = std_msgs::msg::Float64();
+        for (int i = 0; i < 10; i++)
+        {
+            message.data = -i / 100.0;
+            publisher_->publish(message);
+            servo_msg.data = i / 10.0;
+            publisher2_->publish(servo_msg);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
 
-      for (int i = 10; i >=0; i--) {
-        message.data = -i/100.0;
-        publisher_->publish(message);
-        servo_msg.data = i/10.0;
-        publisher2_->publish(servo_msg);
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      }
+        for (int i = 10; i >= 0; i--)
+        {
+            message.data = -i / 100.0;
+            publisher_->publish(message);
+            servo_msg.data = i / 10.0;
+            publisher2_->publish(servo_msg);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;
@@ -52,10 +53,10 @@ class MotorSweepTest : public rclcpp::Node
     size_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MotorSweepTest>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MotorSweepTest>());
+    rclcpp::shutdown();
+    return 0;
 }
