52aee96a55506fc1ed4bb6c85a53677a0a66affb
gabduf <gab.duf04@icloud.com>
2025-11-01 16:41:27 -0400

    pure pursuit corrections

--- pure_pursuit/src/pure_pursuit.hpp
+++ pure_pursuit/src/pure_pursuit.hpp
@@ -14,28 +14,26 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 class PurePursuit : public rclcpp::Node
 {
-    // BELOW ARE PARAMETERS THAT CAN BE TUNED FOR BETTER PERFORMANCE
+    // Below are parameters that need to be tweaked for better performance
     static constexpr double MAX_LOOKAHEAD_DISTANCE_M = 3.5;
     static constexpr double MIN_LOOKAHEAD_DISTANCE_M = 0.4;
-    static constexpr double LOOKAHEAD_DISTANCE_GAIN = 0.25; // speed multiplier for lookahead distance
+    static constexpr double LOOKAHEAD_DISTANCE_GAIN = 0.25;
+    static constexpr double MAX_LOOKAHEAD_FRACTION_OF_PATH = 0.05;
     static constexpr double LOOP_FREQUENCY_HZ = 20.0;
-    static constexpr double WHEELBASE_M = 0.325; // wheelbase of the vehicle in meters
-    static constexpr double MAX_LOOKAHEAD_FRACTION_OF_TRACK = 0.05;
-    static constexpr double EXPONENTIAL_A_CTE = 5.f; // wheelbase of the vehicle in meters
-    static constexpr double EXPONENTIAL_B_CTE = 5.f;   // INPUTS TOPIC AND PATHS
-    static constexpr double EXPONENTIAL_C_CTE = 0.1f;
-    /* This obviously just works in simulation for now. We will want to subscribe to a topic giving us accurate position based 
-    on our chosen localization method(s) */
+    static constexpr double WHEELBASE_M = 0.325;      // Distance between front and rear axles
+    static constexpr double CONSTANT_SPEED_MS = 5.0;  // Constant speed in m/s
+
+    // Topic, input file names and QoS
     static constexpr const uint8_t DEFAULT_QOS = 1;
-    static constexpr const char* CURRENT_POSITION_TOPIC = "/ego_racecar/odom";
     static constexpr const char* DRIVE_CMD_TOPIC = "/drive";
     static constexpr const char* TARGET_WAYPOINT_TOPIC = "/target_waypoint";
+    static constexpr const char* CURRENT_POSITION_TOPIC = "/ego_racecar/odom";
     static constexpr const char* WAYPOINTS_CSV_FILE_NAME = "/sim_ws/src/arcus/resources/waypoints/Spielberg_center.csv";
 
+    // Mathematical constants
     static constexpr const double PI = 3.14159;
     static constexpr const double e = 2.71828;
 
@@ -44,34 +42,29 @@ class PurePursuit : public rclcpp::Node
 
   private:
     void CB_publishDriveCmd(void);
-    void CB_positionSubscriber(const nav_msgs::msg::Odometry& msg);
-    void CB_publishTargetWaypoint(const geometry_msgs::msg::PoseStamped& msg);
+    void CB_publishTargetWaypoint(const geometry_msgs::msg::PoseStamped& msg_);
+    void CB_positionSubscriber(const nav_msgs::msg::Odometry& msg_);
 
     void loadWaypointsFromCSV(void);
     void initRosElements(void);
-    
-    void clipLookaheadDistance(double& _lookAheadDistance);
-    geometry_msgs::msg::PoseStamped getLookaheadPoint(double currentX, double currentY, double lookAheadDistance);
 
-    double speedFromWheelAngle(double& wheelAngle);
+    double clipLookaheadDistance(double lookAheadDistance_) const;
+    geometry_msgs::msg::PoseStamped getLookaheadPoint(const double lookAheadDistance);
 
     double _currentSpeed = 0.0;
     double _currentX = 0.0;
     double _currentY = 0.0;
     double _currentYaw = 0.0;
 
-    size_t _previousBestIndex = 0.0;
-    bool _firstTargetLocked = false;
+    size_t _previousWaypointIndex = 0.0;
+    bool _firstTargetWaypointLocked = false;
 
-    double _lookAheadDistance = 0.0;
-    double _alpha = 0.0;
     std::vector<geometry_msgs::msg::PoseStamped> _waypoints;
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr _positionSubscriber;
     rclcpp::TimerBase::SharedPtr _loopTimer;
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr _driveCmdPublisher;
     rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr _targetWaypointPublisher;
-
 };
 
 #endif  // PURE_PURSUIT_HPP
