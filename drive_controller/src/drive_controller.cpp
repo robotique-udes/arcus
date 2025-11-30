86676cbf419a9e22432975f16d63ec96fd1425b9
Zach3292 <zach3292@hotmail.com>
2025-11-30 12:35:29 -0500

    applied lint

--- drive_controller/src/drive_controller.cpp
+++ drive_controller/src/drive_controller.cpp
@@ -7,38 +7,40 @@
 #include "std_msgs/msg/float64.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
-
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+ * member function as a callback from the timer. */
 
 class DriveController : public rclcpp::Node
 {
   public:
-    DriveController()
-    : Node("motor_sweep_test"), count_(0)
+    DriveController():
+        Node("motor_sweep_test"),
+        count_(0)
     {
-      speed_publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/motor/duty_cycle", 10);
-      steering_publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/servo/position", 10);
-      joy_sub_ = create_subscription<sensor_msgs::msg::Joy>("/joy", rclcpp::QoS{10}, std::bind(&DriveController::joy_callback, this, std::placeholders::_1));
-      RCLCPP_INFO(this->get_logger(), "DriveController node has been started.");
+        speed_publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/motor/duty_cycle", 10);
+        steering_publisher_ = this->create_publisher<std_msgs::msg::Float64>("commands/servo/position", 10);
+        joy_sub_
+            = create_subscription<sensor_msgs::msg::Joy>("/joy",
+                                                         rclcpp::QoS{10},
+                                                         std::bind(&DriveController::joy_callback, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "DriveController node has been started.");
     }
 
   private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
     {
-      auto throttle_msg = std_msgs::msg::Float64();
-      auto steering_msg = std_msgs::msg::Float64();
-      // Binding for ps4 controller
-      float throttle = (joy_msg->axes[5] - joy_msg->axes[2])/2.0; // Assuming axes[5] is throttle and axes[2] is brake
-      float steering = (-joy_msg->axes[3]+1.0)/2.0;
-      throttle_msg.data = throttle;
-      steering_msg.data = steering;
-      speed_publisher_->publish(throttle_msg);
-      steering_publisher_->publish(steering_msg);
+        auto throttle_msg = std_msgs::msg::Float64();
+        auto steering_msg = std_msgs::msg::Float64();
+        // Binding for ps4 controller
+        float throttle = (joy_msg->axes[5] - joy_msg->axes[2]) / 2.0;  // Assuming axes[5] is throttle and axes[2] is brake
+        float steering = (-joy_msg->axes[3] + 1.0) / 2.0;
+        throttle_msg.data = throttle;
+        steering_msg.data = steering;
+        speed_publisher_->publish(throttle_msg);
+        steering_publisher_->publish(steering_msg);
     }
-
 
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr speed_publisher_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr steering_publisher_;
@@ -47,10 +49,10 @@ class DriveController : public rclcpp::Node
     size_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DriveController>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DriveController>());
+    rclcpp::shutdown();
+    return 0;
 }
