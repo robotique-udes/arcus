ba0f775926b976e88b0065570789f8dc3d72a2e2
Zach3292 <zach3292@hotmail.com>
2025-11-30 12:23:00 -0500

    removed vesc and transport driver as they are not in the right repo

--- drive_controller/launch/drive_controller.launch.py
+++ drive_controller/launch/drive_controller.launch.py
@@ -16,14 +16,8 @@ def generate_launch_description():
             executable='controller_node',
             name='controller_node',)
     
-    vesc_dir = get_package_share_directory('vesc_driver')
-    vesc_launch = launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(
-                    vesc_dir + '/launch/vesc_driver_node.launch.py'))
-    
 
     return launch.LaunchDescription([
         joy_node,
         controller_node,
-        vesc_launch,
   ])
