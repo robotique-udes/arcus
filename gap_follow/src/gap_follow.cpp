5e2cabf2245e8786831cb96bbc1b1028e2a3da84
Zach3292 <zach3292@hotmail.com>
2026-01-23 10:58:01 -0500

    cleanup disparity extender demo

--- gap_follow/src/gap_follow.cpp
+++ gap_follow/src/gap_follow.cpp
@@ -22,29 +22,8 @@ ReactiveGapFollow::ReactiveGapFollow():
                                                                  });
 }
 
-void ReactiveGapFollow::preprocessLidar(std::vector<float>& ranges_)
-{
-    size_t rangesSize = ranges_.size();
-
-    for (size_t i = ELIMINATE_EXTREMES_POSITION; i < rangesSize - ELIMINATE_EXTREMES_POSITION; i++)
-    {
-        ranges_[i]
-            = (ranges_[i] + ranges_[i - 1] + ranges_[i - 2] + ranges_[i - 3] + ranges_[i + 1] + ranges_[i + 2] + ranges_[i + 3])
-              / PREPROCESSING_AVG_SAMPLES;
-
-        if (ranges_[i] > MAX_LIDAR_DISTANCE_M)
-        {
-            ranges_[i] = MAX_LIDAR_DISTANCE_M;
-        }
-    }
-
-    return;
-}
-
 void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_)
 {
-    // preprocessing will messup the disparity detection
-    //preprocessLidar(scanMsg_->ranges);
 
     std::vector<float> ranges = scanMsg_->ranges;
     size_t rangesSize = ranges.size();
@@ -58,7 +37,6 @@ void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_
 
     for (size_t i = 0; i < rangesSize; i++)
     {   
-
         if (std::abs(ranges[i] - old_distance) > 0.5)
         {
             uint32_t bubble_distance = static_cast<uint32_t>(2*std::asin(BUBBLE_RADIUS/2*ranges[i])/scanMsg_->angle_increment);
@@ -72,7 +50,6 @@ void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_
             }
         }
         old_distance = ranges[i];
-
     }
 
     for (uint32_t i = 0; i < ELIMINATE_EXTREMES_POSITION; i++)
@@ -85,54 +62,15 @@ void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_
         ranges[i] = 0.0;
     }
 
-    // uint32_t maxGap = 0;
-    // uint32_t gap = 0;
-    // uint32_t startingGapIndex = 0;
-    // uint32_t endingGapIndex = 0;
+    uint32_t maxDistanceIndex = std::distance(ranges.begin(), std::max_element(ranges.begin(), ranges.end()));
 
-    // for (uint32_t i = 0; i < rangesSize; i++)
-    // {
-    //     if (ranges[i] != 0.0)  // Non-zero value: part of a gap
-    //     {
-    //         if (gap == 0)
-    //         {
-    //             // Start of a new gap
-    //             startingGapIndex = i;
-    //         }
-    //         gap++;
-    //         endingGapIndex = i;
-    //     }
-    //     else
-    //     {
-    //         if (gap > maxGap)
-    //         {
-    //             maxGap = gap;
-    //             _maxGapStartingIndex = startingGapIndex;
-    //             _maxGapEndingIndex = endingGapIndex;
-    //         }
-    //         gap = 0;
-    //     }
-    // }
-
-    uint32_t maxDistanceIndex = 0;
-    for (uint32_t i = 0; i <= rangesSize; i++)
-    {
-        // get max distance index
-        if (ranges[i] > ranges[maxDistanceIndex])
-        {
-            maxDistanceIndex = i;
-        }
-    }
-
-    _targetIndex = maxDistanceIndex;
-
-    _targetAngle = scanMsg_->angle_min + _targetIndex * scanMsg_->angle_increment;
+    _targetAngle = scanMsg_->angle_min + maxDistanceIndex * scanMsg_->angle_increment;
 
     ackermann_msgs::msg::AckermannDriveStamped newMsg = ackermann_msgs::msg::AckermannDriveStamped();
 
     newMsg.drive.steering_angle = _targetAngle * OVERSHOOT_FACTOR;
 
-    float targetSpeed = setSpeedFromDistance(ranges[_targetIndex]);
+    float targetSpeed = setSpeedFromDistance(ranges[maxDistanceIndex]);
     newMsg.drive.speed = targetSpeed;
 
     _directionPublisher->publish(newMsg);
