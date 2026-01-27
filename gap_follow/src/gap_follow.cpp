cde46957a1178a340af336ebd11dea91759cc0f1
Zach3292 <zach3292@hotmail.com>
2026-01-27 18:52:53 -0500

    eliminate some data

--- gap_follow/src/gap_follow.cpp
+++ gap_follow/src/gap_follow.cpp
@@ -75,8 +75,12 @@ void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_
         old_distance = ranges[i];
     }
 
+    uint32_t 90deg_index = (M_PI/2 - scanMsg_->angle_min) / scanMsg_->angle_increment;
+    uint32_t neg90deg_index = (-M_PI/2 - scanMsg_->angle_min) / scanMsg_->angle_increment;
+    
+
     uint32_t maxDistanceIndex
-        = std::distance(preprocessedRanges.begin(), std::max_element(preprocessedRanges.begin(), preprocessedRanges.end()));
+        = std::distance(preprocessedRanges.begin() + neg90deg_index, std::max_element(preprocessedRanges.begin() + neg90deg_index, preprocessedRanges.begin() + 90deg_index));
 
     _targetAngle = scanMsg_->angle_min + maxDistanceIndex * scanMsg_->angle_increment;
 
--- gap_follow/src/gap_follow.hpp
+++ gap_follow/src/gap_follow.hpp
@@ -19,9 +19,9 @@ class ReactiveGapFollow : public rclcpp::Node
     static constexpr float MAX_LIDAR_DISTANCE_M = 10.f;
     static constexpr float BUBBLE_RADIUS = 0.5f;
     static constexpr float SPEED_DISTANCE_FACTOR = 0.5f;
-    static constexpr float MAX_SPEED = 7.f;
+    static constexpr float MAX_SPEED = 1.f;
     static constexpr float DISPARITY_THRESHOLD = 0.1f;
-    static constexpr float SAFE_TURNING_DISTANCE = 0.5f;
+    static constexpr float SAFE_TURNING_DISTANCE = 0.1f;
 
     static constexpr const char* LIDAR_SCAN_TOPIC = "/scan";
     static constexpr const char* DRIVE_TOPIC = "/drive";
