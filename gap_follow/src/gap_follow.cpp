d0e22f69ffb6ca1d6cf09e440585cf51a9b81fda
Zach3292 <zach3292@hotmail.com>
2026-01-27 19:28:27 -0500

    fix print again

--- gap_follow/src/gap_follow.cpp
+++ gap_follow/src/gap_follow.cpp
@@ -91,9 +91,9 @@ void ReactiveGapFollow::lidar_CB(sensor_msgs::msg::LaserScan::SharedPtr scanMsg_
     
     maxDistanceIndex += neg90deg_index;
 
-    RCLCPP_INFO(this->get_logger(), "Max distance index: %u, distance: %.2f", maxDistanceIndex, preprocessedRanges[maxDistanceIndex]);
-
     _targetAngle = scanMsg_->angle_min + maxDistanceIndex * scanMsg_->angle_increment;
+
+    RCLCPP_INFO(this->get_logger(), "Max distance angle: %f, distance: %.2f", _targetAngle, preprocessedRanges[maxDistanceIndex]);
 
     // Check for obstacles on the side in the turning direction
     // Check left side (angles > 90 degrees)
