75641d2c55f33af3da9727583f795fea58a89447
Zach3292 <zach3292@hotmail.com>
2026-03-03 19:55:37 -0500

    increase speed

--- gap_follow/src/gap_follow.hpp
+++ gap_follow/src/gap_follow.hpp
@@ -20,7 +20,7 @@ class ReactiveGapFollow : public rclcpp::Node
 
     static constexpr float BUBBLE_RADIUS = 0.25f;
     static constexpr float SPEED_DISTANCE_FACTOR = 0.5f;
-    static constexpr float MAX_SPEED = 3.0f;
+    static constexpr float MAX_SPEED = 4.0f;
     static constexpr float DISPARITY_THRESHOLD = 0.1f;
     static constexpr float SAFE_TURNING_DISTANCE = 0.1f;
 
